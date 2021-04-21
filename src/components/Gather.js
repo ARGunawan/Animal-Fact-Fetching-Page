f7c169b426f3c31ee9a79b68d80f59efcf077630
csangcap <csangcap405@gmail.com>
2021-04-21 16:21:08 -0400

    added the random cat photo call

--- src/components/Gather.js
+++ src/components/Gather.js
@@ -14,16 +14,21 @@ class GetRandom extends Component {
 
     handleSearchClickCat = async () => {
         let linkToCatFact = 'https://cat-fact.herokuapp.com/facts';
+        let linkToCatPic = 'https://aws.random.cat/meow';
 
         try{
             //get the link info
             let response = await axios.get(linkToCatFact);
+            let response_pic = await axios.get(linkToCatPic);
+            //console.log(response_pic.data['file']);
 
             //since the link info only returns 5 different facts, randNum choose one of the five
             let randNum = Math.floor(Math.random() * 5) +1;
             
             //set the state to the random fact
             this.setState({randomFact: response.data[randNum].text, foundFact:true});
+            //set the state to the random cat picture
+            this.setState({randomPicture: response_pic.data['file'], foundPic:true});
             //console.log(this.state.randomFact);
         } catch {
             //does nothing
@@ -41,6 +46,7 @@ class GetRandom extends Component {
                 <button className="search-button" onClick={this.handleSearchClickCat}>Search Cat</button>
                 <br />
                 <p>{this.state.randomFact}</p>
+                <img src={this.state.randomPicture}/>
             </div>
             
 
